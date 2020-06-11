5a49be475d6418ba7a6a8dfd5cdc71901a6eb6f6
Begood <otremoulis@gmail.com>
2020-06-11 23:13:59 +0200

    Add CPN and Seglink

---
+++ CPN/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip3 install gdown
+pip3 install -r requirement.txt
+pip3 install opencv-python
+apt-get update && apt-get install -y libsm6 libxext6
+cd lib
+make clean
+make all
+cd lib_kernel/lib_nms
+./compile.sh
+cd ../../..
